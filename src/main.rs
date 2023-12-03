0923dcaa4e2b0b28ad1b8b5bde2cb34792836975
Cauã Bernardino <cbernardino@live.com>
2023-12-03 12:17:05 +0100

    Start implementing logging and tracing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -6,6 +7,8 @@ use mailer::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("Failed to read config file");
     let pool_conn = PgPool::connect(&config.database.connection_string())
         .await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,16 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let req_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %req_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name,
+    );
+    let _ = request_span.enter();
+    // tracing::info!("Saving new subscriber to the database", req_id);
+
     match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
@@ -20,9 +30,12 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     .execute(db_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("ReqID: {} - New subscriber details have been saved", req_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("ReqID: {} - Failed to execute query: {:?}", req_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
