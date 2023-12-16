2d532b7f421572fb9c387c09956f0f1bd76f76c9
Cauã Bernardino <cbernardino@live.com>
2023-12-16 20:55:47 +0100

    Type driven assertions. Refactor.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,10 @@
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
+
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,7 +13,7 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    name = "Addming a new subscriber",
+    name = "Adding a new subscriber",
     skip(form, db_pool),
     fields(
         subscriber_email = %form.email,
@@ -18,19 +21,37 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&form, &db_pool).await {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let email = match SubscriberEmail::parse(form.0.email) {
+        Ok(email) => email,
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
+
+    let new_subscriber = NewSubscriber { email, name };
+
+    match insert_subscriber(&db_pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[tracing::instrument(name = "Saving new subscriber to the database", skip(form, db_pool))]
-pub async fn insert_subscriber(form: &FormData, db_pool: &PgPool) -> Result<(), sqlx::Error> {
+#[tracing::instrument(
+    name = "Saving new subscriber to the database",
+    skip(new_subscriber, db_pool)
+)]
+pub async fn insert_subscriber(
+    db_pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now(),
     )
     .execute(db_pool)
@@ -40,4 +61,17 @@ pub async fn insert_subscriber(form: &FormData, db_pool: &PgPool) -> Result<(), 
         e
     })?;
     Ok(())
+}
+
+/// Returns `true` if the input satisfies the validation constraints on acceptable subscriber
+/// names, returns `false` otherwise.
+pub fn is_valid_name(s: &str) -> bool {
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_chars = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_chars = s.chars().any(|g| forbidden_chars.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_chars)
 }
